eb745eb1603c33edc6ba3cf9e0b274b11d0b50e4
Oleksii Khoroshun <alexeyhoroshunvk@gmail.com>
2019-12-05 22:54:44 +0200

    Addd ListView to show data.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,23 +10,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
- Future<List<Data>> getAllData() async{
-   const String api='https://jsonplaceholder.typicode.com/photos';
-   final data = await http.get(api);
-   dynamic jsonData = json.decode(data.body);
-   List<Data>listOf =[];
-   for(dynamic i in jsonData){
-     final Data data = Data(i['id'], i['title'], i['url'], i['thumbnailUrl']);
-     listOf.add(data);
-   }
-   return listOf;
- }
+  Future<List<Data>> getAllData() async {
+    const String api = 'https://jsonplaceholder.typicode.com/photos';
+    final data = await http.get(api);
+    dynamic jsonData = json.decode(data.body);
+    List<Data> listOf = [];
+    for (dynamic i in jsonData) {
+      final Data data = Data(i['id'], i['title'], i['url'], i['thumbnailUrl']);
+      listOf.add(data);
+    }
+    return listOf;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Json Parsing App'),
         backgroundColor: Colors.deepOrange,
@@ -79,13 +77,81 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               title: const Text('Close'),
               leading: Icon(Icons.close, color: Colors.red),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).pop();
               },
-
             ),
           ],
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            height: 250.0,
+            child: FutureBuilder(
+              future: getAllData(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot == null) {
+                  return Center(
+                    child: const Text('Loading data'),
+                  );
+                } else {
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        elevation: 10.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.network(
+                              snapshot.data[index].url,
+                              height: 150.0,
+                              width: 150.0,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(height: 7.0),
+                            Container(
+                              height: 50.0,
+                              margin: const EdgeInsets.all(6.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.red,
+                                      child: Text(
+                                          snapshot.data[index].id.toString()),
+                                      foregroundColor: Colors.white,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 6.0,
+                                  ),
+                                  Container(
+                                    width: 80.0,
+                                    child: Text(
+                                      snapshot.data[index].title,
+                                      maxLines: 1,
+                                      style: TextStyle(
+                                        color: Colors.deepOrange
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
